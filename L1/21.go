7111b4f635f8fc4f118936a7826dba478a411e11
Sidio01 <sidio1992@gmail.com>
2022-05-04 22:53:13 +0300

    completed L1

--- L1/21.go
+++ L1/21.go
@@ -1,9 +1,62 @@
-// 21. Реализовать паттерн «адаптер» на любом примере.
-
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"regexp"
+	"sort"
+)
+
+type wordCounter struct{}
+
+func (wc *wordCounter) countWords(sentence string) map[string]int {
+	re := regexp.MustCompile("\\W+")
+	words := re.Split(sentence, -1)
+	// words := strings.Split(sentence, re)
+
+	result := make(map[string]int)
+	for _, word := range words {
+		if _, ok := result[word]; ok {
+			result[word]++
+		} else {
+			result[word] = 1
+		}
+	}
+	return result
+}
+
+type textProcessor struct{}
+
+func (tp *textProcessor) processText(sys system, s string) {
+	sys.getProcessedText(s)
+}
+
+type system interface {
+	getProcessedText(s string)
+}
+
+type wordCounterAdapter struct {
+	wc *wordCounter
+}
+
+func (wca *wordCounterAdapter) getProcessedText(s string) {
+	m := wca.wc.countWords(s)
+	keys := make([]string, 0, len(m))
+	for key := range m {
+		keys = append(keys, key)
+	}
+	sort.SliceStable(keys, func(i, j int) bool {
+		return m[keys[i]] < m[keys[j]]
+	})
+	for _, k := range keys {
+		fmt.Println(k, m[k])
+	}
+}
 
 func main() {
-	fmt.Println("in progress")
+	s := "work in progress, progress in progress"
+
+	tp := &textProcessor{}
+	wc := &wordCounter{}
+	wca := &wordCounterAdapter{wc: wc}
+	tp.processText(wca, s)
 }
--- L1/6.go
+++ L1/6.go
@@ -1,9 +1,70 @@
-// 6. Реализовать все возможные способы остановки выполнения горутины.
-
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"sync"
+	"time"
+)
 
+func contextCancel(c context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case <-c.Done():
+			fmt.Println("goroutine stop by cancel")
+			time.Sleep(time.Millisecond * 10)
+			return
+		}
+	}
+}
+
+func contextTimeout(c context.Context, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case <-c.Done():
+			fmt.Println("goroutine stop by timeout")
+			time.Sleep(time.Millisecond * 10)
+			return
+		}
+	}
+}
+
+func chanel(c chan struct{}, wg *sync.WaitGroup) {
+	defer wg.Done()
+	for {
+		select {
+		case <-c:
+			fmt.Println("goroutine stop by channel")
+			time.Sleep(time.Millisecond * 10)
+			return
+		}
+	}
+}
 func main() {
-	fmt.Println("in progress")
+	ctx := context.Background()
+	wg := &sync.WaitGroup{}
+	t := time.Second * time.Duration(5)
+
+	ctxCancel, cancel := context.WithCancel(ctx)
+	ctxTimeout, _ := context.WithTimeout(ctx, t)
+	ch := make(chan struct{})
+
+	fmt.Println("goroutine start by cancel")
+	wg.Add(1)
+	go contextCancel(ctxCancel, wg)
+	fmt.Println("goroutine start by timeout")
+	wg.Add(1)
+	go contextTimeout(ctxTimeout, wg)
+	fmt.Println("goroutine start by channel")
+	wg.Add(1)
+	go chanel(ch, wg)
+
+	select {
+	case <-time.After(t):
+		cancel()
+		ch <- struct{}{}
+	}
+	wg.Wait()
 }
